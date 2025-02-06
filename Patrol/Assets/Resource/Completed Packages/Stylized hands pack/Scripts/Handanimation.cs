a113abf38f1e57f61d978d0c0b899d69bc954fb2
hamellen <zzodong0309@naver.com>
2025-02-07 00:06:10 +0900

    asset

---
+++ Patrol/Assets/Resource/Completed Packages/Stylized hands pack/Scripts/Handanimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Handanimation : MonoBehaviour
+{
+    [Tooltip("Don't forget to assign the prefiew animator!")]
+    public Animator anim;
+
+    [Tooltip("Set this integer to preview the animations, ranges from 0 - 14 for a total of 15 animations!")]
+    public int animationindex;
+  
+    void Update()
+    {
+            AnimationIndex();
+            SetAnimation();
+    }
+
+    private int AnimationIndex()
+    {
+        return Mathf.Clamp(animationindex,0,14);
+    }
+
+    private void SetAnimation()
+    {
+        anim.SetInteger("animationIndex", AnimationIndex());
+    }
+}
